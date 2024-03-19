44f4dd06c920bf55eb456fad95677105115a4d04
yassinemontassar <sawthegamer70@gmail.com>
2024-03-19 01:53:37 +0100

    cron api

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,13 +1,12 @@
 import { Menu } from "@prisma/client";
 import prisma from "@/app/lib/db";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 type Data = {
   result: Menu[];
 };
 
-export async function GET(req: NextApiRequest, res: NextApiResponse<Data>) {
+export async function GET(req: Request, res: NextResponse<Data>) {
   const result = await prisma.menu.findMany({
     where: {
       name: "cron",
