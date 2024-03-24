0ffd5f7dad16eb0ebfd55d89ad0516647973a351
yassinemontassar <sawthegamer70@gmail.com>
2024-03-24 23:34:12 +0100

    animation fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -35,7 +35,7 @@ const Hero = ({
       <ScrollAnimationWrapper>
         <motion.div
           className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8"
-          variants={scrollAnimation}
+         
         >
           <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
             <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
@@ -51,7 +51,7 @@ const Hero = ({
             </Button>
           </div>
           <div className="flex w-full">
-            <motion.div className="h-full w-full" variants={scrollAnimation}>
+            <motion.div className="h-full w-full" >
               <Image
                 src="/images/hero/hero.jpg"
                 alt="logo_menurapide"
