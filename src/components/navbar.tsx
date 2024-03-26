9c6acdf5ff75b9b7958f703e47e4a39d5df6e921
yassinemontassar <sawthegamer70@gmail.com>
2024-03-26 03:38:48 +0100

    UI update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <div>
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between  lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
