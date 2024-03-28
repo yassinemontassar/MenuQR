12e2aa44f1b6594a0df41a922187f84184547ce2
yassinemontassar <sawthegamer70@gmail.com>
2024-03-28 02:43:44 +0100

    translation

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useMemo } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -21,14 +21,14 @@ const Pricing = () => {
               variants={scrollAnimation}
               className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed"
             >
-              Choose Your Plan
+              Choisissez votre forfait
             </motion.h3>
             <motion.p
               variants={scrollAnimation}
               className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
             >
-              Lets choose the package that is best for you and explore it happily
-              and cheerfully.
+              Choisissons le forfait qui vous convient le mieux et explorez-le
+              joyeusement et avec entrain.
             </motion.p>
           </ScrollAnimationWrapper>
           <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
@@ -37,10 +37,10 @@ const Pricing = () => {
                 variants={scrollAnimation}
                 className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
                 whileHover={{
-                  scale : 1.1,
+                  scale: 1.1,
                   transition: {
-                    duration: .2
-                  }
+                    duration: 0.2,
+                  },
                 }}
               >
                 <div className="p-4 lg:p-0 mt-6 lg:mt-16">
@@ -81,46 +81,46 @@ const Pricing = () => {
                 variants={scrollAnimation}
                 className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
                 whileHover={{
-                  scale : 1.1,
+                  scale: 1.1,
                   transition: {
-                    duration: .2
-                  }
+                    duration: 0.2,
+                  },
                 }}
               >
-              <div className="p-4 lg:p-0 mt-6 lg:mt-16">
-                <Image
-                  src="/images/pricing/Standard.png"
-                  width={145}
-                  height={165}
-                  alt="Standard Plan"
-                />
-              </div>
-              <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
-                Standard Plan{" "}
-              </p>
-              <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
-                <li className="relative check custom-list my-2">
-                  Unlimited Bandwitch
-                </li>
-                <li className="relative check custom-list my-2">
-                  Encrypted Connection
-                </li>
-                <li className="relative check custom-list my-2">
-                  No Traffic Logs
-                </li>
-                <li className="relative check custom-list my-2">
-                  Works on All Devices
-                </li>
-                <li className="relative check custom-list my-2">
-                  Connect Anyware{" "}
-                </li>
-              </ul>
-              <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
-                <p className="text-2xl text-black-600 text-center mb-4 ">
-                  $9 <span className="text-black-500">/ mo</span>
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/images/pricing/Standard.png"
+                    width={145}
+                    height={165}
+                    alt="Standard Plan"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  Standard Plan{" "}
                 </p>
-                <Button>Select</Button>
-              </div>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    Unlimited Bandwitch
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Encrypted Connection
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    No Traffic Logs
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Works on All Devices
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Connect Anyware{" "}
+                  </li>
+                </ul>
+                <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                  <p className="text-2xl text-black-600 text-center mb-4 ">
+                    $9 <span className="text-black-500">/ mo</span>
+                  </p>
+                  <Button>Select</Button>
+                </div>
               </motion.div>
             </ScrollAnimationWrapper>
             <ScrollAnimationWrapper className="flex justify-center">
@@ -128,50 +128,50 @@ const Pricing = () => {
                 variants={scrollAnimation}
                 className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
                 whileHover={{
-                  scale : 1.1,
+                  scale: 1.1,
                   transition: {
-                    duration: .2
-                  }
+                    duration: 0.2,
+                  },
                 }}
               >
-              <div className="p-4 lg:p-0 mt-6 lg:mt-16">
-                <Image
-                  src="/images/pricing/Premium.png"
-                  width={145}
-                  height={165}
-                  alt="Premium Plan"
-                />
-              </div>
-              <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
-                Premium Plan{" "}
-              </p>
-              <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
-                <li className="relative check custom-list my-2">
-                  Unlimited Bandwitch
-                </li>
-                <li className="relative check custom-list my-2">
-                  Encrypted Connection
-                </li>
-                <li className="relative check custom-list my-2">
-                  No Traffic Logs
-                </li>
-                <li className="relative check custom-list my-2">
-                  Works on All Devices
-                </li>
-                <li className="relative check custom-list my-2">
-                  Connect Anyware{" "}
-                </li>
-                <li className="relative check custom-list my-2">
-                  Get New Features{" "}
-                </li>
-              </ul>
-              <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
-                <p className="text-2xl text-black-600 text-center mb-4 ">
-                  $12 <span className="text-black-500">/ mo</span>
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/images/pricing/Premium.png"
+                    width={145}
+                    height={165}
+                    alt="Premium Plan"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  Premium Plan{" "}
                 </p>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    Unlimited Bandwitch
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Encrypted Connection
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    No Traffic Logs
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Works on All Devices
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Connect Anyware{" "}
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Get New Features{" "}
+                  </li>
+                </ul>
+                <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                  <p className="text-2xl text-black-600 text-center mb-4 ">
+                    $12 <span className="text-black-500">/ mo</span>
+                  </p>
 
-                <Button>Select</Button>
-              </div>
+                  <Button>Select</Button>
+                </div>
               </motion.div>
             </ScrollAnimationWrapper>
           </div>
