7611731018d0f16aa9485849c3e6d34adf0eff89
yassinemontassar <sawthegamer70@gmail.com>
2024-04-26 00:41:46 +0100

    Terms

--- src/components/SignInWithGoogle.tsx
+++ src/components/SignInWithGoogle.tsx
@@ -8,7 +8,7 @@ import { FcGoogle } from "react-icons/fc";
 export default function SigninWithGoogle() {
   return (
     <>
-      <div className="flex flex-col items-center space-y-3">
+      <div className="flex flex-col items-center justify-center space-y-4">
         <Button
           onClick={() =>
             signIn("google", {
@@ -21,9 +21,11 @@ export default function SigninWithGoogle() {
           Google
           <FcGoogle className="w-4 h-4 ml-4" />
         </Button>
-        <div className="space-x-2">
-          <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-            En créant un compte, vous confirmez avoir lu et compris notre
+        <div >
+          <p className="text-center text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            En créant un compte
+            <div> vous confirmez avoir lu et compris notre
+            </div>
             <Link href="https://www.menurapide.tn/terms" target="_blank" className="text-blue-600 underline">
               {" "} Politique de Confidentialité
             </Link>
