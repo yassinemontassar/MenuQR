9e519a7607985cedff34867de853c44657260a04
yassinemontassar <sawthegamer70@gmail.com>
2024-04-26 23:45:12 +0100

    header target

--- src/app/(website)/website/componenets/header.tsx
+++ src/app/(website)/website/componenets/header.tsx
@@ -5,7 +5,7 @@ export default async function Header() {
     return (
         <header className=" py-4 shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-          <Link href="/" target="_blank"
+          <Link href="/" 
            className="font-bold text-3xl bg-gradient-to-r from-orange-600 to-orange-300 text-transparent bg-clip-text hover:cursor-pointer ">
             MenuRapide 
           </Link>
