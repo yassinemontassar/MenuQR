d2af7586f7d21d5643fd284f8ee60a9fe12aca53
yassinemontassar <sawthegamer70@gmail.com>
2024-05-01 13:13:06 +0100

    robots

--- actions/editUser.tsx
+++ actions/editUser.tsx
@@ -1,17 +1,14 @@
 "use server";
 import prisma from "@/app/lib/db";
-export const editUser = async (formData: FormData) => 
-
-{
-    const id = formData.get('id') as string;
-    const name = formData.get('name') as string;
-    await prisma.user.update({
-        where: {
-            id
-        },
-        data: {
-            name
-        },
-      });
-   
+export const editUser = async (formData: FormData) => {
+  const id = formData.get("id") as string;
+  const name = formData.get("name") as string;
+  await prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+    },
+  });
 };
