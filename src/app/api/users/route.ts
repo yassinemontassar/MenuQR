a0cfd34c33b8c92ec7988274800c7aabfaeb65ee
yassinemontassar <sawthegamer70@gmail.com>
2024-05-21 22:41:32 +0100

    origin removal

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/app/lib/db";
 import { NextResponse } from "next/server";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://menurapideadmin.netlify.app",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
