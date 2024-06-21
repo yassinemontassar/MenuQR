cf45ac35f8f71ca79a8a8e10604aad5f63e02db2
yassinemontassar <sawthegamer70@gmail.com>
2024-06-21 23:37:31 +0100

    UI/UX improvements

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,13 @@
 
+import { auth } from "@/app/utils/auth";
+import DropDown from "./DropDown";
 import SignInForm from "./SignInForm";
 import Navbar from "./navbar";
-import DropDown from "./DropDown";
-import { auth } from "@/app/utils/auth";
 
 export default async function Header() {
   const session = await auth()
     return (
-        <header className="fixed  w-full  z-10  transition-all bg-background shadow-md p-4 pt-3">
+      <header className="sticky z-[100] inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all p-4 pt-3 dark:bg-gray-800/20 dark:border-gray-700">
         <div className="container mx-auto px-4 flex items-center justify-between">
           <div className="flex-1">
             <Navbar />
