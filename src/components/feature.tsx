a5f2ca9420fc2d24caffd0fc69d977fdb10a19c3
yassinemontassar <sawthegamer70@gmail.com>
2024-06-22 13:59:31 +0100

    UI/UX improvements

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -2,11 +2,14 @@
 import getScrollAnimation from "@/app/utils/getScrollAnimation";
 import { motion } from "framer-motion";
 import { Star } from "lucide-react";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 import { useMemo } from "react";
 import { Button } from "./ui/button";
-import Illu from "/public/images/feature/Illustration2.webp";
+import light from "/public/votremenu.png";
+import dark from "/public/votremenu_dark.png";
+
 const features = [
   "QR Code Généré Automatiquement.",
   "Aperçu en Temps Réel sur Téléphone Mobile.",
@@ -16,23 +19,36 @@ const features = [
 
 const Feature = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
-
+  const {  theme } = useTheme()
+  const logoToUse = theme === "dark" ? dark : light;
   return (
     <div
-      className="max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      className="max-w-screen-xl mt-8 mb-6 sm:mt-20 sm:mb-20 px-6 sm:px-8 lg:px-16 mx-auto"
       id="feature"
     >
       <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
         <div className="flex w-full justify-end">
-          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
-            <Image
-              src={Illu}
-              alt="feature_logo"
-              placeholder="blur"
-              sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
-            />
+          <motion.div
+            className="-mt-10 w-full h-full"
+            variants={scrollAnimation}
+          >
+            <div className="relative mx-auto border-gray-800 dark:border-gray-300 bg-gray-300 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] ">
+              <div className="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-300 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
+              <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-300 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+              <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-300 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+              <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-300 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+              <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-300">
+                <Image
+                  src={logoToUse}
+                  className="absolute lg:w-52 left-56 -top-24 select-none hidden sm:block lg:hidden xl:block"
+                  alt=""
+                />
+                <iframe
+                  className=" w-[282px] h-[572px]"
+                  src="https://www.menurapide.tn/website/726f4840-01df-430f-b753-892af40a136c"
+                ></iframe>
+              </div>
+            </div>
           </motion.div>
         </div>
         <div>
@@ -73,10 +89,10 @@ const Feature = () => {
             variants={scrollAnimation}
           >
             <Button className="text-xl sm:text-2xl bg-gradient-to-r from-orange-600 to-orange-300 gap-2 ">
-            <Link href="https://www.menurapide.tn/website/726f4840-01df-430f-b753-892af40a136c">
-            Aperçu en direct
-          </Link>
-            <Star />
+              <Link href="https://www.menurapide.tn/website/726f4840-01df-430f-b753-892af40a136c">
+                Aperçu en direct
+              </Link>
+              <Star />
             </Button>
           </motion.div>
         </div>
