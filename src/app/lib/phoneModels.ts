8c4568e0406784de233d0b83ae5505e57a367139
yassinemontassar <sawthegamer70@gmail.com>
2024-06-24 13:50:30 +0100

    phone model preview functionality

---
+++ src/app/lib/phoneModels.ts
@@ -0,0 +1,17 @@
+export interface PhoneModel {
+  value: string;
+  label: string;
+}
+export const phoneModels: PhoneModel[] = [
+    { value: "Nexus 5", label: "Nexus 5" },
+  { value: "iPhone X", label: "iPhone X" },
+  { value: "iPhone 8", label: "iPhone 8" },
+  { value: "iPhone 8 Plus", label: "iPhone 8 Plus" },
+  { value: "iPhone 5s", label: "iPhone 5s" },
+  { value: "iPhone 5c", label: "iPhone 5c" },
+  { value: "iPhone 4s", label: "iPhone 4s" },
+  { value: "Samsung Galaxy S5", label: "Samsung Galaxy S5" },
+  { value: "HTC One", label: "HTC One" },
+  { value: "iPad Mini", label: "iPad Mini" },
+  { value: "Galaxy Note 8", label: "Galaxy Note 8" },
+];
--- src/components/device.tsx
+++ src/components/device.tsx
@@ -1,22 +1,17 @@
-"use client"
-import Image from 'next/image';
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { DeviceFrameset } from 'react-device-frameset';
-import 'react-device-frameset/styles/marvel-devices.min.css';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from './ui/select';
+"use client";
+import { phoneModels } from "@/app/lib/phoneModels";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { DeviceFrameset } from "react-device-frameset";
+import "react-device-frameset/styles/marvel-devices.min.css";
+import { useMediaQuery } from "react-responsive";
 interface DevicePreviewProps {
   keyProp: boolean; // Assuming keyProp is a boolean, change the type accordingly
 }
 
 const DevicePreview: React.FC<DevicePreviewProps> = ({ keyProp }) => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
   const [reloadCounter, setReloadCounter] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const params = useParams();
@@ -44,54 +39,53 @@ const DevicePreview: React.FC<DevicePreviewProps> = ({ keyProp }) => {
     };
   }, [keyProp]); // Include keyProp in the dependency array to trigger the effect on key change
 
+  const [selectedPhone, setSelectedPhone] = useState(phoneModels[0].value) ; // Default first model
+  const handleChange = (newValue: any) => {
+    setSelectedPhone(newValue);
+    console.log(newValue);
+  };
 
+  
   return (
     <>
-    <div className='flex flex-col items-center gap-6'>
-      <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a model" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Model</SelectLabel>
-          {/* <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem> */}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-   
-      <DeviceFrameset device="Nexus 5" color="black">
-        <div className="h-full overflow-y-scroll">
-          {isLoading ? (
-            // Display a loading message while content is being reloaded
-            <div className="bg-background flex items-center justify-center h-full">
-            <Image
-            src="/images/gif/scan.gif"
-            alt="gif"
-            quality={100}
-            height={120}
-            width={120}
-            priority
-            className='rounded-xl animate-bounce'
-            unoptimized
-          />
+      <div className="flex flex-col items-center gap-6">
+          <select className="p-2 border rounded-md" value={selectedPhone} onChange={(e) => handleChange(e.target.value)}>
+            {phoneModels.map((model) => (
+              <option key={model.value} value={model.value}>
+                {model.label}
+              </option>
+            ))}
+          </select>
+           {/* @ts-ignore */}    
+        <DeviceFrameset device={selectedPhone}  width={isTabletOrMobile ? 300 : undefined}  >
+          <div className="h-full overflow-y-scroll">
+            {isLoading ? (
+              // Display a loading message while content is being reloaded
+              <div className="bg-background flex items-center justify-center h-full">
+                <Image
+                  src="/images/gif/scan.gif"
+                  alt="gif"
+                  quality={100}
+                  height={120}
+                  width={120}
+                  priority
+                  className="rounded-xl animate-bounce"
+                  unoptimized
+                />
+              </div>
+            ) : (
+              // Render the iframe when not in loading state
+              <iframe
+                key={reloadCounter}
+                width="100%"
+                height="100%"
+                src={`https://www.menurapide.tn/website/${params.menuId}`}
+              ></iframe>
+            )}
           </div>
-
-          ) : (
-            // Render the iframe when not in loading state
-            <iframe key={reloadCounter} width="100%" height="100%" src={`${process.env.NEXT_PUBLIC_BASE_URL}/website/${params.menuId}`}></iframe>
-
-          )}
-        </div>
-
-        
-      </DeviceFrameset>
+        </DeviceFrameset>
       </div>
-  </>
+    </>
   );
-}
+};
 export default DevicePreview;
