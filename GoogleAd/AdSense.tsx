5fcac57ef0949775ca07f9cda8e1935d75d05752
yassinemontassar <sawthegamer70@gmail.com>
2024-06-27 00:42:26 +0100

    adsense

---
+++ GoogleAd/AdSense.tsx
@@ -0,0 +1,17 @@
+import Script from "next/script";
+
+type AdsenseTypes = {
+    pId: string;
+}
+
+const AdSense = ({ pId }: AdsenseTypes) => {
+    return (
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        />
+    )
+}
+export default AdSense;
